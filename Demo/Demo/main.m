0fd09d37caa402aa9ab508c769a864da0de43bb7
James <chongyangfly@gmail.com>
2016-06-07 16:40:41 +0800

    add author information

--- Demo/Demo/main.m
+++ Demo/Demo/main.m
@@ -2,6 +2,11 @@
 //  main.m
 //  Demo
 //
+//  email：chongyangfly@163.com
+//  QQ：1909295866
+//  github：https://github.com/wangcy90
+//  blog：http://wangcy90.github.io
+//
 //  Created by WangChongyang on 16/6/3.
 //  Copyright © 2016年 WangChongyang. All rights reserved.
 //
